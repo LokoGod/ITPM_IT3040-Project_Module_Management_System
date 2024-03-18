57b074a3ab8a0c89fc094ed86fa5169947dbc238
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-03-18 15:43:38 +0530

    ui

--- client/src/app/students/groupRegistration/page.tsx
+++ client/src/app/students/groupRegistration/page.tsx
@@ -1,7 +1,30 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+} from "@chakra-ui/react";
+
+import { FaAngleRight, FaHouse } from "react-icons/fa6";
+
 export default function GroupRegistration() {
-    return (
-        <>
-        
-        </>
-    )
-};
+  return (
+    <>
+      <div className="text-xs m-2">
+        <Breadcrumb spacing="8px" separator={<FaAngleRight color="gray.500" />}>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">
+              <FaHouse />
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/students/">For Students</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="">Group Registration</BreadcrumbLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+      </div>
+    </>
+  );
+}
---
+++ client/src/app/students/page.tsx
@@ -0,0 +1,9 @@
+export default function Students() {
+    return (
+        <>
+        <div>
+            <h1>Hello</h1>
+        </div>
+        </>
+    )
+};
