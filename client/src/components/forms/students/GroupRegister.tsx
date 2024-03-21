c1844897d160330a7cab846ab3708877e86d8d51
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-03-21 17:33:31 +0530

    ui

--- client/src/components/forms/students/GroupRegister.tsx
+++ client/src/components/forms/students/GroupRegister.tsx
@@ -37,21 +37,21 @@ import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 import { redirect, useRouter } from "next/navigation";
 
-import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons"
- 
-import { cn } from "@/lib/utils"
+import { CaretSortIcon, CheckIcon } from "@radix-ui/react-icons";
+
+import { cn } from "@/lib/utils";
 import {
   Command,
   CommandEmpty,
   CommandGroup,
   CommandInput,
   CommandItem,
-} from "@/components/ui/command"
+} from "@/components/ui/command";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
+} from "@/components/ui/popover";
 import { useState } from "react";
 
 const frameworks = [
@@ -75,59 +75,69 @@ const frameworks = [
     value: "astro",
     label: "Astro",
   },
-]
+];
+
+//  stu name, regi num, contact num, email, batch, special, (leader details),
+// pro title, research area, research group, select supervisors & co-supervisors
 
 const formSchema = z.object({
-  jobError: z.string().min(2, {
+  stuName: z.string().min(2, {
     message: "Error must be at least 2 characters.",
   }),
-  jobDescription: z.string().min(2, {
-    message: "Description must be at least 2 characters.",
+  stuRegiNum: z.string().min(2, {
+    message: "registration number must be at least 2 characters.",
   }),
-  jobIncludedItems: z.string().min(1, {
+  stuContactNum: z.string().min(1, {
     message: "an item must be at least 1 characters",
   }),
-  model: z.string().min(2, {
+  stuEmail: z.string().min(2, {
     message: "Model must be at least 2 characters.",
   }),
-  category: z.string().min(2, {
+  stuBatch: z.string().min(2, {
     message: "Category must be at least 2 characters.",
   }),
-  cusName: z.string().min(2, {
+  stuSpecial: z.string().min(2, {
     message: "Name must be at least 2 characters.",
   }),
-  phoneNum: z.string().min(10, {
+  proTitle: z.string().min(10, {
     message: "Phone-num must have 10 numericals.",
   }),
-  phoneNum2: z
+  proResearchArea: z
     .string()
     .min(10, {
       message: "Phone-num must have 10 numericals.",
     })
     .optional(),
-  email: z
+  proResearchGroup: z
     .string()
     .min(2, {
       message: "email must be at least 2 characters.",
     })
     .optional(),
-  // income_category: z.string({
-  //   required_error: "Please select a category!",
-  // }),
-  // recurring: z.boolean().default(false).optional(),
-  // receving_date: z.date().optional(),
+  supervisor: z
+    .string()
+    .min(2, {
+      message: "email must be at least 2 characters.",
+    })
+    .optional(),
+  co_supervisor: z
+    .string()
+    .min(2, {
+      message: "email must be at least 2 characters.",
+    })
+    .optional(),
 });
 
 export function GroupRegister() {
   const router = useRouter();
-  const [open, setOpen] = useState(false)
-  const [value, setValue] = useState("")
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      jobError: "",
+      //   jobError: "",
     },
   });
 
@@ -149,28 +159,60 @@ export function GroupRegister() {
 
   return (
     <div>
-      <Card className="w-[800px] mx-auto">
+      <Card className="w-auto mx-10">
         <CardHeader>
-          <CardTitle>Job Form</CardTitle>
+          <CardTitle>Registration Form</CardTitle>
           <CardDescription>
-            Enter the details of the recieved income
+            Enter the details of your group members
           </CardDescription>
         </CardHeader>
         <CardContent>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <div className="grid grid-cols-2 gap-4">
+              <div className="grid grid-cols-8 gap-4">
                 <div className="col-span-2">
                   <FormField
                     control={form.control}
-                    name="jobError"
+                    name="stuName"
                     render={({ field }) => (
                       <FormItem>
-                        <FormLabel>Error</FormLabel>
+                        <FormLabel>stuName</FormLabel>
                         <FormControl>
                           <Input {...field} />
                         </FormControl>
-                        <FormDescription>Summarize the error</FormDescription>
+                        <FormDescription>stuName</FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="col-span-1">
+                  <FormField
+                    control={form.control}
+                    name="stuRegiNum"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>stuRegiNum</FormLabel>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                        <FormDescription>stuRegiNum</FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="col-span-1">
+                  <FormField
+                    control={form.control}
+                    name="stuContactNum"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>stuContactNum</FormLabel>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                        <FormDescription>stuContactNum</FormDescription>
                         <FormMessage />
                       </FormItem>
                     )}
@@ -179,14 +221,83 @@ export function GroupRegister() {
                 <div className="col-span-2">
                   <FormField
                     control={form.control}
-                    name="jobDescription"
+                    name="stuEmail"
                     render={({ field }) => (
                       <FormItem>
-                        <FormLabel>Job/Error description</FormLabel>
+                        <FormLabel>stuEmail</FormLabel>
                         <FormControl>
-                          <Textarea {...field} />
+                          <Input {...field} />
                         </FormControl>
-                        <FormDescription>Describe the job</FormDescription>
+                        <FormDescription>stuEmail</FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="col-span-1">
+                  <FormField
+                    control={form.control}
+                    name="stuBatch"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>stuBatch</FormLabel>
+                        <FormControl>
+                          <Select
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          >
+                            <SelectTrigger className="">
+                              <SelectValue placeholder="Select" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectItem value="Laptop">Regular</SelectItem>
+                              <SelectItem value="Desktop">June</SelectItem>
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="col-span-1">
+                  <FormField
+                    control={form.control}
+                    name="stuSpecial"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>stuSpecial</FormLabel>
+                        <FormControl>
+                          <Select
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          >
+                            <SelectTrigger className="">
+                              <SelectValue placeholder="Select" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectItem value="Laptop">IT</SelectItem>
+                              <SelectItem value="Desktop">CSNE</SelectItem>
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </div>
+              <div className="grid grid-cols-4 gap-4">
+                <div className="">
+                  <FormField
+                    control={form.control}
+                    name="proTitle"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>proTitle</FormLabel>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
                         <FormMessage />
                       </FormItem>
                     )}
@@ -195,10 +306,10 @@ export function GroupRegister() {
                 <div className="">
                   <FormField
                     control={form.control}
-                    name="model"
+                    name="proResearchArea"
                     render={({ field }) => (
                       <FormItem>
-                        <FormLabel>Device Model</FormLabel>
+                        <FormLabel>proResearchArea</FormLabel>
                         <FormControl>
                           <Input {...field} />
                         </FormControl>
@@ -210,10 +321,25 @@ export function GroupRegister() {
                 <div className="">
                   <FormField
                     control={form.control}
-                    name="category"
+                    name="proResearchGroup"
                     render={({ field }) => (
                       <FormItem>
-                        <FormLabel>Device Category</FormLabel>
+                        <FormLabel>proResearchGroup</FormLabel>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="">
+                  <FormField
+                    control={form.control}
+                    name="supervisor"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>supervisor</FormLabel>
                         <FormControl>
                           <Select
                             onValueChange={field.onChange}
@@ -239,76 +365,32 @@ export function GroupRegister() {
                     )}
                   />
                 </div>
-                <div className="col-span-2">
-                  <FormField
-                    control={form.control}
-                    name="jobIncludedItems"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Included</FormLabel>
-                        <FormControl>
-                          <Input {...field} />
-                        </FormControl>
-                        <FormDescription></FormDescription>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
                 <div className="">
                   <FormField
                     control={form.control}
-                    name="cusName"
+                    name="co_supervisor"
                     render={({ field }) => (
                       <FormItem>
-                        <FormLabel>Customer Name</FormLabel>
+                        <FormLabel>co-supervisor</FormLabel>
                         <FormControl>
-                          <Input {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <div className="">
-                  <FormField
-                    control={form.control}
-                    name="phoneNum"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Customer Phone Number</FormLabel>
-                        <FormControl>
-                          <Input type="number" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <div className="">
-                  <FormField
-                    control={form.control}
-                    name="phoneNum2"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Additional Phone Number *</FormLabel>
-                        <FormControl>
-                          <Input type="number" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <div className="">
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Email *</FormLabel>
-                        <FormControl>
-                          <Input type="email" {...field} />
+                          <Select
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                          >
+                            <SelectTrigger className="">
+                              <SelectValue placeholder="Select" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectItem value="Laptop">Laptop</SelectItem>
+                              <SelectItem value="Desktop">Desktop</SelectItem>
+                              <SelectItem value="Mobile">Mobile</SelectItem>
+                              <SelectItem value="Server">Server</SelectItem>
+                              <SelectItem value="SparePart">
+                                Spare Part
+                              </SelectItem>
+                              <SelectItem value="Other">Other</SelectItem>
+                            </SelectContent>
+                          </Select>
                         </FormControl>
                         <FormMessage />
                       </FormItem>
@@ -316,6 +398,7 @@ export function GroupRegister() {
                   />
                 </div>
               </div>
+
               <Button type="submit">Add</Button>
             </form>
           </Form>
