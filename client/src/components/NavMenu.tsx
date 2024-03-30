191c26c4c43f884936d20c02ed32c0ad2879e80a
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-03-31 00:11:51 +0530

    ui

--- client/src/components/NavMenu.tsx
+++ client/src/components/NavMenu.tsx
@@ -15,41 +15,12 @@ import {
 } from "@/components/ui/navigation-menu"
 import { FaRegMoon } from "react-icons/fa";
 
-const components: { title: string; href: string; description: string }[] = [
+const adminStaff: { title: string; href: string; description: string }[] = [
   {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
+    title: "User Registration",
+    href: "/adminStaff/userRegistration",
     description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+      "Register staff & students into the system.",
   },
 ]
 
@@ -107,16 +78,16 @@ export function NavMenu() {
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+          <NavigationMenuTrigger>Admin Staff</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
+              {adminStaff.map((adminStaff) => (
                 <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
+                  key={adminStaff.title}
+                  title={adminStaff.title}
+                  href={adminStaff.href}
                 >
-                  {component.description}
+                  {adminStaff.description}
                 </ListItem>
               ))}
             </ul>
