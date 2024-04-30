83819f491d33e865dbbf8fbcc46b9be28f7b2033
Mayukha Palihena <mpansuja007@gmail.com>
2024-04-30 07:02:47 +0000

    commit

--- client/src/app/(main)/page.tsx
+++ client/src/app/(main)/page.tsx
@@ -57,7 +57,7 @@ export default async function Home() {
                 </CardDescription>
               </CardHeader>
               <CardContent>
-                <HomeTable />
+                {/*<HomeTable />*/}
               </CardContent>
             </Card>
           </div>
