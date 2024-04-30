1046cbbb7055716c53a1c0bc047033245f849cbf
Mayukha Palihena <mpansuja007@gmail.com>
2024-04-30 11:28:45 +0000

    auth init

--- server/repositories/adminRepository.js
+++ server/repositories/adminRepository.js
@@ -1,12 +1,26 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
+import bcrypt from "bcrypt";
+
 const getAllAdminDetails = async () => {
   return await prisma.admin.findMany();
 };
 
-const createAdmin = async () => {
-  return await prisma.admin.create({});
+const createAdmin = async (username, password) => {
+  if (!username || !password) {
+    console.log("Username & Password Mandatory");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  return await prisma.admin.create({
+    data: {
+      username,
+      password: hash,
+    },
+  });
 };
 
 const adminRepository = {
