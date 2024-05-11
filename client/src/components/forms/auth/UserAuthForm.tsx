717f4a40ea1c25fdcfddd2f3739137fe5ba5a76d
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-05-12 00:44:00 +0530

    ui

--- client/src/components/forms/auth/UserAuthForm.tsx
+++ client/src/components/forms/auth/UserAuthForm.tsx
@@ -66,12 +66,12 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
         </div>
         <div className="relative flex justify-center text-xs uppercase">
           <span className="bg-background px-2 text-muted-foreground">
-            Or if you are a student
+            Forgot credentials ?
           </span>
         </div>
       </div>
       <Button variant="outline" type="button" disabled={isLoading}>
-        Student Sign In
+        Raise a ticket !
       </Button>
     </div>
   );
